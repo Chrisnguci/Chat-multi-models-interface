9b0e7107ad0eb2dc23af5e63c49d6c9244acb8fc
Chrisnguci <toilatuancuong@gmail.com>
2025-02-17 00:04:39 +0700

    reduce functions

--- interface.py
+++ interface.py
@@ -1,18 +1,29 @@
 from models import ModelChain
 import gradio as gr
 
-def get_gemini_response(models,user_input, system_prompt):
-    if models != "gemini-1.5-flash":
+DEEPSEEK_MODEL = "deepseek/deepseek-r1:free"
+GEMINI_MODEL = "google/gemini-2.0-flash-exp:free"
+QWEN_MODEL="qwen/qwen2.5-vl-72b-instruct:free"
+
+def get_models_response(models,user_input,system_prompt):
+    if len(models) >1:
         return "Currently Unsupported"
     else:
-    	print(f"bạn đang sử dụng Model {models}")
-    	chain= ModelChain()
-    	return chain.get_gemini_response(user_input, system_prompt)
+        print(f"Reponse using model {models}")
+        chain = ModelChain()
+        if models[0]=="deepseek-r1":
+            return chain.get_model_response(DEEPSEEK_MODEL,user_input,system_prompt)
+        elif models[0]=="gemini-2.0-flash-exp":
+            return chain.get_model_response(GEMINI_MODEL,user_input,system_prompt)
+        elif models[0]=="qwen2.5-vl-72b-instruct":
+            return chain.get_model_response(QWEN_MODEL,user_input,system_prompt)
+        else:
+            return "Current Unsupported"
 
 def main():
 	view = gr.Interface(
-		fn= get_gemini_response,
-        inputs = [ gr.Dropdown(["o3-mini","gemini-1.5-flash","claude-3-5-sonnet-20241022"], label = "Response model", value = "gemini-1.5-flash"),gr.Textbox(label = "Your input",lines = 10, placeholder = "Nhập nội dung"), gr.Textbox(label = "Nhiệm vụ của Bot", placeholder = "Vd: bạn là một chuyên gia thương mại điện tử 10 năm kinh nghiệm hãy giúp tôi trả lời các câu hỏi sau") ], 
+		fn= get_models_response,
+        inputs = [gr.CheckboxGroup(["gemini-2.0-flash-exp","deepseek-r1","qwen2.5-vl-72b-instruct"], label = "Response model", value = "deepseek-r1"),gr.Textbox(label = "Your input",lines = 10, placeholder = "Nhập nội dung"), gr.Textbox(label = "Nhiệm vụ của Bot", placeholder = "Vd: bạn là một chuyên gia thương mại điện tử 10 năm kinh nghiệm hãy giúp tôi trả lời các câu hỏi sau")], 
         outputs = gr.Textbox(label ="Output", lines = 26),
         flagging_mode = "never",
         stop_btn = gr.Button("Stop",variant = "stop",visible = True),
@@ -20,3 +31,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
--- models.py
+++ models.py
@@ -3,46 +3,33 @@ import os
 import openai
 from dotenv import load_dotenv
 # MOELS
-DEEPSEEK_MODEL = "deepseek-reasoner"
-GEMINI_MODEL = "gemini-1.5-flash"
-
+DEEPSEEK_MODEL = "deepseek/deepseek-r1:free"
+GEMINI_MODEL = "google/gemini-2.0-flash-exp:free"
+QWEN_MODEL="qwen/qwen2.5-vl-72b-instruct:free"
+OPENROUTER_API_KEY="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"
 load_dotenv()
 class ModelChain:
 	def __init__(self):
-		self.gemini_client = openai.OpenAI(
-			api_key = os.getenv("GEMINI_API_KEY"),
-			base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
-			)
-		self.deepseek_client = openai.OpenAI(
- 			api_key = os.getenv("DEEPSEEK_API_KEY"),
-            base_url = "https://api.deepseek.com"
-            )
+		self.client = self.generate_client(OPENROUTER_API_KEY,"https://openrouter.ai/api/v1" )
 		self.deepseek_messages = []
 		self.gemini_messages = []
-	def  get_gemini_response(self,user_input, system_prompt):
-		messages = [{
-			"role": "system",
-			"content": system_prompt
-					},
-					{
-			"role": "user",
-			"content": user_input
-		}
-		]
+
+	def generate_client(self,api_key, url):
+		return openai.OpenAI(
+			api_key = api_key,
+			base_url = url,
+			) 
+	def get_model_response(self,model,user_input,system_prompt):
+		messages = [{"role":"system","content": system_prompt},
+					{"role":"user","content":user_input}]
 		try:
-			stream = self.gemini_client.chat.completions.create(
-				model = GEMINI_MODEL,
-				messages= messages,
-				stream = True)
-			full_response = ""
-			for chunk in stream :
-				full_response += chunk.choices[0].delta.content
-			return full_response
+			result = self.client.chat.completions.create(
+				model = model,
+				messages = messages,
+				)
+			return result.choices[0].message.content
 		except Exception as e:
-			return "Error occurred while getting response"
-
-
-
+			return f"Error occurred while getting response{e}"
 def main():
 	chain= Modelschain()
 
